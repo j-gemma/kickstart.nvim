2d75de0f5e7a73db21c454ec1109164b4e7982fa
j-gemma <jacobg391@gmail.com>
2026-03-19 10:18:20 -0400

    Re-refix treesitter

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -19,8 +19,16 @@ return {
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
+      -- Check the operating system
+      local is_windows = vim.loop.os_uname().sysname == 'Windows_NT'
+
+      if is_windows then
+        ---@diagnostic disable-next-line: missing-fields
+        require('nvim-treesitter.configs').setup(opts)
+      else
+        ---@diagnostic disable-next-line: missing-fields
+        require('nvim-treesitter.config').setup(opts)
+      end
 
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
